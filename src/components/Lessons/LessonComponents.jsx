817d25cb3d04b2922cf729c562a5265781a0dda1
IyaadDev <113124292+IyaadDev@users.noreply.github.com>
2023-10-27 16:42:02 +0400

    bug patches

--- src/components/Lessons/LessonComponents.jsx
+++ src/components/Lessons/LessonComponents.jsx
@@ -1,4 +1,39 @@
-// This is a reading bit...
+import React, { useEffect } from "react";
+
+function FetchLessonDataL({}) {
+  let lessonData = null;
+
+  const getLessonIdFromURL = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const lessonId = urlParams.get('id');
+    console.log('Lesson ID:', lessonId);
+    return lessonId;
+  };
+
+  useEffect(() => {
+    const fetchLessonData = async () => {
+      const lessonIdNo = getLessonIdFromURL();
+      var lessonId = lessonIdNo + ".json";
+      if (lessonId) {
+        try {
+          const response = await fetch(`https://cloud.acroford.com/sct/lessons/${lessonId}`);
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          lessonData = await response.json();
+          console.log('Lesson data:', lessonData);
+          // Further logic with lessonData can be implemented here
+        } catch (error) {
+          console.error('Error fetching lesson data:', error);
+        }
+      }
+    };
+
+    fetchLessonData();
+  }, []);
+
+  return <div></div>;
+}
 
 function InfoL ({}) {
 }
@@ -14,19 +49,5 @@ function StartL ({}) {
 
 function EndL ({}) {
 }
-
-function FetchLessonDataL({}) {
-    const getLessonIdFromURL = () => {
-      const urlParams = new URLSearchParams(window.location.search);
-      const lessonId = urlParams.get('id');
-      console.log('Lesson ID:', lessonId);
-      return lessonId;
-    };
-  
-    // Call the function to extract and log the lesson ID
-    getLessonIdFromURL();
-  
-    return <div>Fetching lesson data...</div>;
-  }
 
 export { InfoL, QuizL, ResultsL, StartL, EndL, FetchLessonDataL };
